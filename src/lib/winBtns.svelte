faa18154b26851df08412c831dad231729649644
patrick-uos <patrick@outlook.com>
2023-09-12 19:46:35 +0800

    dd

--- src/lib/winBtns.svelte
+++ src/lib/winBtns.svelte
@@ -19,7 +19,7 @@
       if(osType==="Darwin"){
         await hide();
       }else{
-        await appWindow.hide();
+        await appWindow.minimize();
       }
       }} class="w-6 h-6 rounded-md items-center flex hover:bg-slate-400 hover:fill-white justify-center" id="titlebar-minimize">
       <svg class="h-3 w-3" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"  width="200" height="200"><path d="M960 544H64a32 32 0 1 1 0-64h896a32 32 0 1 1 0 64"></path></svg>
